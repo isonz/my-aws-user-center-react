9e0d7a7a4d8223ca59928b1c7e2694ff0cb7cdc3
ison <isonz@qq.com>
2020-09-06 13:17:35 +0800

    add router-config

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
 import { storeRedux } from './redux/store.redux';
-import {Router} from 'react-router-dom'
 import { Provider } from 'react-redux';
-import Routers from './routers'
+import PrivateRoute from './routers'
 import { history } from './helpers/history';
+import {Router, Route, Switch, Redirect } from 'react-router-dom';
+import { TestPage } from './pages/test.page';
 
 
 function App() {
   console.log(process.env.API_HOST);
   return (
-      <Provider store={storeRedux}>
-        <Router history={history}>
-          <Routers/>
-        </Router>
-      </Provider>
+      <div className="jumbotron">
+          <div className="container">
+              <div className="col-sm-8 col-sm-offset-2">
+                  {alert.message &&
+                  <div className={`alert ${alert.type}`}>{alert.message}</div>
+                  }
+                  <Router>
+                      <Route path="/" component={TestPage} />
+                  </Router>
+              </div>
+          </div>
+      </div>
   );
 }
 
---
+++ src/pages/test.page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class TestPage extends React.Component {
+
+    render() {
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h2>Login</h2>
+            </div>
+        );
+    }
+}
