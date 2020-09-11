7987ec032bea7f046c61e21852d9bb8a78487c96
ison <isonz@qq.com>
2020-09-12 00:15:47 +0800

    asdf

--- src/components/auth/auth.action.js
+++ src/components/auth/auth.action.js
@@ -1,7 +1,6 @@
 import { authConstants } from './auth.constant';
 import { AuthService } from './auth.service';
 import { AlertActions } from '../alert/alert.action';
-import { history } from '../../helpers/history';
 
 export class AuthActions {
 
@@ -9,56 +8,9 @@ export class AuthActions {
         return {type: authConstants.LOGIN_REQUEST, loginUser}
     }
 
-
-    // static login(username, password) {
-    //     return dispatch => {
-    //         dispatch(request({ username } ) );
-    //
-    //         AuthService.login(username, password)
-    //             .then(
-    //                 user => {
-    //                     dispatch(success(user));
-    //                     history.push('/');
-    //                 },
-    //                 error => {
-    //                     dispatch(failure(error.toString()));
-    //                     dispatch(AlertActions.error(error.toString()));
-    //                 }
-    //             );
-    //     };
-    //
-    //     function request(user) { return { type: authConstants.LOGIN_REQUEST, user } }
-    //     function success(user) { return { type: authConstants.LOGIN_SUCCESS, user } }
-    //     function failure(error) { return { type: authConstants.LOGIN_FAILURE, error } }
-    // }
-
-
     static register(regUser) {
         return { type: authConstants.REGISTER_REQUEST, regUser };
     }
-
-    // static register(user) {
-    //     return dispatch => {
-    //         dispatch(request(user));
-    //
-    //         AuthService.register(user)
-    //             .then(
-    //                 user => {
-    //                     dispatch(success());
-    //                     history.push(process.env.REACT_APP_LOGIN_ENTRANCE);
-    //                     dispatch(AlertActions.success('Registration successful'));
-    //                 },
-    //                 error => {
-    //                     dispatch(failure(error.toString()));
-    //                     dispatch(AlertActions.error(error.toString()));
-    //                 }
-    //             );
-    //     };
-    //
-    //     function request(user) { return { type: authConstants.REGISTER_REQUEST, user } }
-    //     function success(user) { return { type: authConstants.REGISTER_SUCCESS, user } }
-    //     function failure(error) { return { type: authConstants.REGISTER_FAILURE, error } }
-    // }
 
     static logout() {
         AuthService.logout();
@@ -83,3 +35,71 @@ export class AuthActions {
         function failure(id, error) { return { type: authConstants.DELETE_FAILURE, id, error } }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+/**
+static login(username, password) {
+    return dispatch => {
+        dispatch(request({ username } ) );
+        AuthService.login(username, password)
+            .then(
+                user => {
+                    dispatch(success(user));
+                    history.push('/');
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(AlertActions.error(error.toString()));
+                }
+            );
+    };
+    function request(user) { return { type: authConstants.LOGIN_REQUEST, user } }
+    function success(user) { return { type: authConstants.LOGIN_SUCCESS, user } }
+    function failure(error) { return { type: authConstants.LOGIN_FAILURE, error } }
+}
+
+static register(user) {
+    return dispatch => {
+        dispatch(request(user));
+        AuthService.register(user)
+            .then(
+                user => {
+                    dispatch(success());
+                    history.push(process.env.REACT_APP_LOGIN_ENTRANCE);
+                    dispatch(AlertActions.success('Registration successful'));
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(AlertActions.error(error.toString()));
+                }
+            );
+    };
+    function request(user) { return { type: authConstants.REGISTER_REQUEST, user } }
+    function success(user) { return { type: authConstants.REGISTER_SUCCESS, user } }
+    function failure(error) { return { type: authConstants.REGISTER_FAILURE, error } }
+}
+
+static _delete(id) {
+    return dispatch => {
+        dispatch(request(id));
+        AuthService.delete(id)
+            .then(
+                user => dispatch(success(id)),
+                error => dispatch(failure(id, error.toString()))
+            );
+    };
+    function request(id) { return { type: authConstants.DELETE_REQUEST, id } }
+    function success(id) { return { type: authConstants.DELETE_SUCCESS, id } }
+    function failure(id, error) { return { type: authConstants.DELETE_FAILURE, id, error } }
+}
+
+ */
