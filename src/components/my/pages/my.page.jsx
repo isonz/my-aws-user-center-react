5e087c88fd46b69f7bbbaf6d41ba570450bb7520
ison <isonz@qq.com>
2020-10-05 20:32:16 +0800

    asf

--- src/components/my/pages/my.page.jsx
+++ src/components/my/pages/my.page.jsx
@@ -3,6 +3,26 @@ import './my.page.css';
 import {getLocalUser} from "../../../helpers/local";
 import {MyActions} from "../my.action";
 import {connect} from "react-redux";
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+
+
+function getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+        message.error('You can only upload JPG/PNG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+        message.error('Image must smaller than 2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+}
 
 class MyPage extends React.Component {
 
@@ -12,6 +32,7 @@ class MyPage extends React.Component {
             user: getLocalUser(),
             email : '',
             avatar: '',
+            loading: false,
         };
         this.onBlurHandle = this.onBlurHandle.bind(this);
         this.onChangeHandle = this.onChangeHandle.bind(this);
@@ -27,21 +48,39 @@ class MyPage extends React.Component {
         if('email' === name){
             this.props.item.email = value;
         }
-        if('avatar' === name){
-            this.props.item.avatar = value;
+
+        if (info.file.status === 'uploading') {
+            this.setState({ loading: true });
+            return;
         }
-        //console.log(this.props.item);
+        if (info.file.status === 'done') {
+            // Get this url from response in real world.
+            getBase64(info.file.originFileObj, imageUrl =>
+                this.setState({
+                    imageUrl,
+                    loading: false,
+                }),
+            );
+        }
+
     }
 
     onBlurHandle(e) {
         const { name, value } = e.target;
         // console.log(name, value);
         this.setState({ [name]: value });
-        this.props.myUpdate({[name]: value });
+        this.props.myUpdate(this.props.item);
     }
 
     render() {
         const { alertMsg, alertType, loading, item, updating } = this.props;
+
+        const uploadButton = (
+            <div>
+                {loading ? <LoadingOutlined /> : <PlusOutlined />}
+                <div style={{ marginTop: 8 }}>Upload</div>
+            </div>
+        );
 
         return (
             <div className='my-page'>
@@ -53,20 +92,18 @@ class MyPage extends React.Component {
                 <h1>{this.state.user.user.username}</h1>
                 <p>ID: {this.state.user.user.id}</p>
 
-                <form>
 
-                    <div className="form-group ant-avatar-group">
-                        <img id='avatar_show' src={item?.avatar || '/images/avatar_default.jpg'} alt='avatar' />
-                        <input type="file" id="avatar" name='avatar' />
-                        <p>Click me to modify</p>
-                    </div>
+                <div className="form-group ant-avatar-group">
+                    <img id='avatar_show' src={item?.avatar || '/images/avatar_default.jpg'} alt='avatar' />
+                    <input type="file" id="avatar" name='avatar' onChange={this.onChangeHandle} ref={this.fileInput} />
+                    <p>Click me to modify</p>
+                </div>
 
-                    <div className="form-group">
-                        <label htmlFor="name">E-mail</label>
-                        <input type="text" className="form-control" id="email" name='email' placeholder="E-mail" value={item?.email || ''} onBlur={this.onBlurHandle} onChange={this.onChangeHandle} />
-                    </div>
 
-                </form>
+                <div className="form-group">
+                    <label htmlFor="name">E-mail</label>
+                    <input type="text" className="form-control" id="email" name='email' placeholder="E-mail" value={item?.email || ''} onBlur={this.onBlurHandle} onChange={this.onChangeHandle} />
+                </div>
 
             </div>
         );
